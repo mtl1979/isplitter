297f1ac8be7b9dbb96c967812afcce39a50f203b
Mika Lindqvist <postmaster@raasu.org>
2016-04-03 15:52:29 +0300

    Convert result of qgetenv to unicode.

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ main( int argc, char** argv )
 	// Set our working directory
 
 #ifdef _WIN32
-	QString datadir = qgetenv("APPDATA");
+	QString datadir = QString::fromLocal8Bit(qgetenv("APPDATA").constData());
 	QDir dir(datadir);
 	dir.mkdir("Image Splitter");
 	datadir = MakePath(datadir, "Image Splitter");
@@ -137,7 +137,7 @@ NoTranslation:
 	// Qt's own translator file
 	QFileInfo qfi(lfile);
 	QString qt_lang = QString::null;
-	QString qtdir = qgetenv("QTDIR");
+	QString qtdir = QString::fromLocal8Bit(qgetenv("QTDIR").constData());
 	langfile = qfi.fileName().replace(QRegExp("isplitter"), "qt");
 
 	if (qtdir != QString::null)
